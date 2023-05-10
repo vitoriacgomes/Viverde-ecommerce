cfc343aaee25ce866124cb27ff8807250aa1fde9
vitoria <vi21cgomes@gmail.com>
2023-05-10 10:35:43 -0300

    Home init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './componentes/header';
+import Home from './modulos/home';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
     </div>
   );
 }
