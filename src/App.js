c7292573040e6e9121f649cbd4213302538ddd9b
vitoria <vi21cgomes@gmail.com>
2023-05-18 21:29:57 -0300

    login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Footer from './componentes/Footer';
 import GoogleLogin from 'react-google-login';
 
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
   const [currentPage, setCurrentPage] = useState('home');
 
   function goToProdutos() {
@@ -20,6 +23,13 @@ function App() {
 
   return (
     <div>
+      <GoogleLogin
+        clientId="196785399714-9ivm5jat0du8q607h7sp2p5gk5a2iada.apps.googleusercontent.com"
+        buttonText="Login com Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
       <Header onHomeButtonClick={goToHome} onProdutosButtonClick={goToProdutos} />
       {currentPage === 'home' && <Home onProdutosButtonClick={goToProdutos} />}
       {currentPage === 'produtos' && <Produtos />}
